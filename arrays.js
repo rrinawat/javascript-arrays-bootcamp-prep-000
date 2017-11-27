99f0347694452f5401f35aa7edd4add86fb9a4ec
Ryan Inawat <rrinawat@gmail.com>
2017-11-27 23:17:03 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var chocolateBars =  ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element){
+  array.unshift(`${element}`)
+}
+
+function addElementToEndOfArray(array, element){
+  array.push(`${element}`)
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array[0] = `${element}`
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array[array.length] = `${element}`
+}
