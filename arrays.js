9b6494142f784e693c07246c1cb7b4f91120db64
Ryan Inawat <rrinawat@gmail.com>
2017-11-28 00:32:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,6 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array[0] = element
+  array[0] = [element]
   return array
 }
