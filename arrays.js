f26e0baf366de19552837ee14b01109b2abaff92
Ryan Inawat <rrinawat@gmail.com>
2017-11-28 01:10:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,7 +24,7 @@ function accessElementInArray(array, index){
   return array[index]
 }
 //remove first element
-function removeElementFromBeginningOfArray(array){
+function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
   return array
 }
